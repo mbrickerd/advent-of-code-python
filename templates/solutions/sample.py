fd3067f218e74ee1b6e916b451e90c8fa67ea104
mallory.brickerd <mallory.brickerd@eraneos.com>
2024-12-06 16:07:38 +0100

    Templates

---
+++ templates/solutions/sample.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from aoc.models.base import SolutionBase
+
+
+class Solution(SolutionBase):
+    def part1(self, data: List[str]) -> int:
+        return
+
+    def part2(self, data: List[str]) -> int:
+        return
