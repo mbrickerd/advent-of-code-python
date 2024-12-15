2edb4ff88f49af168de395e0b48792485fc9f525
mallory.brickerd <mallory.brickerd@eraneos.com>
2024-12-15 13:12:06 +0100

    Day 14 tests

---
+++ tests/test_14.py
@@ -0,0 +1,10 @@
+from aoc.models.tester import TestSolutionUtility
+
+
+def test_day14_part1():
+    TestSolutionUtility.run_test(
+        day=14,
+        is_raw=False,
+        part_num=1,
+        expected=12,
+    )
