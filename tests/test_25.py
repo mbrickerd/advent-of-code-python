ec57be179e0746566c0ec2c6a71804d3a1e71465
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-02-19 14:04:47 +0100

    Day 25 tests

---
+++ tests/test_25.py
@@ -0,0 +1,10 @@
+from aoc.models.tester import TestSolutionUtility
+
+
+def test_day25_part1():
+    TestSolutionUtility.run_test(
+        day=25,
+        is_raw=False,
+        part_num=1,
+        expected=3,
+    )
